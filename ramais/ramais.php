b6a688c5030513f2064b04cde6121686f2cac417
SC LUCINO <lucino2@gmail.com>
2021-05-26 17:09:11 -0300

    testando pesquisa de ramais

---
+++ ramais/ramais.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Ramais</title>
+<script src="jquery-1.7.2.min.js"></script>
+<script type="text/javascript" charset="utf-8" language="javascript" src="jquery.dataTables.js"></script>
+<script type="text/javascript" charset="utf-8" language="javascript" src="DT_bootstrap.js"></script>
+    </head>
+<body>
+
+<table cellpadding="0" cellspacing="0" border="1" id="ramais" class="ramaisx">
+    <thead>
+        <tr>
+            <th width="10%">Divisão</th>
+            <th>Nome</th>
+            <th>Seção</th>
+            <th width="70px">Ramal</th>
+            <th>Email</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="trTitlex">
+            <td style="border: white; background-color: #ff6666"></td>
+            <td style="text-align: left; font-weight: bolder; border: white; background-color: #ff6666">OUTROS RAMAIS</td>
+            <td style="border: white; background-color: #ff6666"></td>
+            <td style="border: white; background-color: #ff6666"></td>
+            <td style="border: white; background-color: #ff6666"></td>
+        </tr>
+        <tr style="text-align: center">
+            <td>-</td>
+            <td style="text-align:left">Permanência X</td>
+            <td>-</td>
+            <td style="text-align:center">3471</td>
+            <td>-</td>
+        </tr>
+        <tr style="text-align: center">
+            <td>-</td>
+            <td style="text-align:left">Permanência Y</td>
+            <td>-</td>
+            <td style="text-align:center">3779</td>
+            <td>-</td>
+        </tr>
+        <tr style="text-align: center">
+            <td>-</td>
+            <td style="text-align:left">Lanchonete</td>
+            <td>-</td>
+            <td style="text-align:center">3772</td>
+            <td>-</td>
+        </tr>
+        <tr style="text-align: center">
+            <td>-</td>
+            <td style="text-align:left">Posto Médico</td>
+            <td>-</td>
+            <td style="text-align:center">3384</td>
+            <td>-</td>
+        </tr>
+        <tr style="text-align: center">
+            <td>-</td>
+            <td style="text-align:left">Segurança</td>
+            <td>-</td>
+            <td style="text-align:center">3414 / 3427</td>
+            <td>-</td>
+        </tr>
+        <tr style="text-align: center">
+            <td>-</td>
+            <td style="text-align:left">Portaria B</td>
+            <td>-</td>
+            <td style="text-align:center">3064 / 3047</td>
+            <td>-</td>
+        </tr>
+        <tr style="text-align: center">
+            <td>-</td>
+            <td style="text-align:left">Portaria C</td>
+            <td>-</td>
+            <td style="text-align:center">3027 / 3194</td>
+            <td>-</td>
+        </tr>
+        <tr style="text-align: center">
+            <td>-</td>
+            <td style="text-align:left">Central Telefônica</td>
+            <td>-</td>
+            <td style="text-align:center">2432 / 2616</td>
+            <td>-</td>
+        </tr>
+	<tr style="text-align: center">
+            <td>-</td>
+            <td style="text-align:left">Brigada de Inc&ecirc;ncido</td>
+            <td>-</td>
+            <td style="text-align:center">3078</td>
+            <td>-</td>
+        </tr>
+	<tr style="text-align: center">
+            <td>-</td>
+            <td style="text-align:left">Sala de Reunião</td>
+            <td>-</td>
+            <td style="text-align:center">3710</td>
+            <td>-</td>
+        </tr>
+    </tbody>
+</table>
+
+    </body>
+</html>
